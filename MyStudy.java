9e46592ee961e126c9ab44b23f7cd02854bcee94
mr.tan <121086062@qq.com>
2024-02-28 19:13:00 +0800

    git 简单使用

---
+++ MyStudy.java
@@ -0,0 +1,16 @@
+import java.util.Properties;
+
+public class MyStudy {
+    public static void main(String[] args) {
+        Runnable runnable = new MyRunnable();
+        new Thread(runnable).start();
+    }
+
+    private static class MyRunnable implements Runnable {
+
+        public void run(){
+            Properties properties = System.getProperties();
+            System.out.println(properties);
+        }
+    }
+}
