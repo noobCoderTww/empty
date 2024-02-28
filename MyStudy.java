39af065e86bd39829d492c6078ae0d0913aa5b7d
mr.tan <121086062@qq.com>
2024-02-28 19:21:58 +0800

    git 简单使用

--- MyStudy.java
+++ MyStudy.java
@@ -1,17 +1,17 @@
-import java.util.Properties;
 
 public class MyStudy {
     public static void main(String[] args) {
-        Runnable runnable = new MyRunnable();
+        var runnable = new MyRunnable();
         new Thread(runnable).start();
+        new Thread(() -> System.out.println("Hello World")).start();
         var name = "tan";
-         System.out.println(name + " age:18");
+        System.out.println(name + " age:18");
     }
 
     private static class MyRunnable implements Runnable {
 
-        public void run(){
-            Properties properties = System.getProperties();
+        public void run() {
+            var properties = System.getProperties();
             System.out.println(properties);
         }
     }
